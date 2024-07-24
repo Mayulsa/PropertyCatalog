96e108ada4bae6b5dad8410f0d425686b7f0f27e
maicol <mailopezsantos1998@gmail.com>
2024-07-24 17:17:45 -0600

    Add project files.

---
+++ PropertyCatalog/Models/Propiedad.cs
@@ -0,0 +1,18 @@
+﻿namespace PropertyCatalog.Models
+{
+    public class Propiedad
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal BasePrice { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string GetFormattedBasePrice() => BasePrice.ToString("0.00");
+
+    }
+}
---
+++ PropertyCatalog/Models/TodoItem.cs
@@ -0,0 +1,8 @@
+﻿namespace PropertyCatalog.Models
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public bool IsComplete { get; set; } = false;
+    }
+}
