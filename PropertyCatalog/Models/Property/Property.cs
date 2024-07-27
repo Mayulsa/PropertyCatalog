7ea18bcb5cf91e238b476996c1459f41c338dbc5
maicol <mailopezsantos1998@gmail.com>
2024-07-26 18:15:34 -0600

    Add Web API and definiendo las 2 app una para el sito web y otra para managment.

--- PropertyCatalog/Models/Property/Property.cs
+++ PropertyCatalog/Models/Property/Property.cs
@@ -1,6 +1,24 @@
-﻿namespace PropertyCatalog.Models.Property
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PropertyCatalog.Models.Property
 {
     public class Property
     {
+
+        public string? Name { get; set; }
+        public string? Location { get; set; }
+        public string? Size { get; set; }
+        public decimal Price { get; set; }
+        public string? Description { get; set; }
+        public string GetFormattedBasePrice() => Price.ToString("0.00");
+        /*
+          Solar en Venta Frente al Parque Mirador Norte.
+
+           Son 2,207 m2
+
+           Precio Por metro RD$ 16,000 Pesos.
+
+           Bien ubicado y con su titulo.
+        */
     }
 }
